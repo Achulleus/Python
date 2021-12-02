da93f21f0fa11a7c173564a32b96a46429b0afe9
Achilleus <56840977+Achulleus@users.noreply.github.com>
2021-12-02 09:51:36 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ while True:
             sql = "INSERT INTO sspes (spieler, npc) VALUES (%s, %s)"
             val = (wahl, ai)
             mycursor.execute(sql, val)
-            mydb.comit
+            mydb.commit()
             break
 
     if wahl == ai:
@@ -58,7 +58,7 @@ while True:
         sql = "INSERT INTO hs (spielergewonnen, npcgewonnen) VALUES (%s, %s)"
         val = (True, False)
         mycursor.execute(sql, val)
-        mydb.comit
+        mydb.commit()
 
     else:
         print("{0:10}{1:10}".format("Spieler: ", wahl[0].upper() + wahl[1:]))
@@ -68,17 +68,89 @@ while True:
         sql = "INSERT INTO hs (spielergewonnen, npcgewonnen) VALUES (%s, %s)"
         val = (False, True)
         mycursor.execute(sql, val)
-        mydb.comit
+        mydb.commit()
 
     show = input("\nSoll die Statistik ausgegeben werden? (j/n): ")
     if show == "j" or show == "n":
-        mycursor.execute("SELECT count(spieler), count(npc) FROM sspes")
+        print("Der Spieler hat so oft Spock genommen:")
+        mycursor.execute('SELECT count(spieler) FROM sspes where spieler = "spock";')
         myresult = mycursor.fetchall()
 
         for x in myresult:
             print(x)
 
-        mycursor.execute("SELECT count(spielergewonnen), count(npcgewonnen) FROM hs")
+        print("Der Spieler hat so oft Schere genommen:")
+        mycursor.execute('SELECT count(spieler) FROM sspes where spieler = "schere";')
+        myresult = mycursor.fetchall()
+
+        for x in myresult:
+            print(x)
+
+        print("Der Spieler hat so oft Stein genommen:")
+        mycursor.execute('SELECT count(spieler) FROM sspes where spieler = "stein";')
+        myresult = mycursor.fetchall()
+
+        for x in myresult:
+            print(x)
+
+        print("Der Spieler hat so oft Papier genommen:")
+        mycursor.execute('SELECT count(spieler) FROM sspes where spieler = "papier";')
+        myresult = mycursor.fetchall()
+
+        for x in myresult:
+            print(x)
+
+        print("Der Spieler hat so oft Echse genommen:")
+        mycursor.execute('SELECT count(spieler) FROM sspes where spieler = "echse";')
+        myresult = mycursor.fetchall()
+
+        for x in myresult:
+            print(x)
+
+        print("Der Computer hat so oft Spock genommen:")
+        mycursor.execute('SELECT count(npc) FROM sspes where npc = "spock";')
+        myresult = mycursor.fetchall()
+
+        for x in myresult:
+            print(x)
+
+        print("Der Computer hat so oft Schere genommen:")
+        mycursor.execute('SELECT count(npc) FROM sspes where npc = "schere";')
+        myresult = mycursor.fetchall()
+
+        for x in myresult:
+            print(x)
+
+        print("Der Cmputer hat so oft Stein genommen:")
+        mycursor.execute('SELECT count(npc) FROM sspes where npc = "stein";')
+        myresult = mycursor.fetchall()
+
+        for x in myresult:
+            print(x)
+
+        print("Der Computer hat so oft Papier genommen:")
+        mycursor.execute('SELECT count(npc) FROM sspes where npc = "papier";')
+        myresult = mycursor.fetchall()
+
+        for x in myresult:
+            print(x)
+
+        print("Der Computer hat so oft Echse genommen:")
+        mycursor.execute('SELECT count(npc) FROM sspes where npc = "echse";')
+        myresult = mycursor.fetchall()
+
+        for x in myresult:
+            print(x)
+
+        print("So oft hat der Spieler gewonnen:")
+        mycursor.execute("SELECT count(spielergewonnen) FROM hs")
+        myresult = mycursor.fetchall()
+
+        for x in myresult:
+            print(x)
+
+        print("so oft hat der Computer gewonnen:")
+        mycursor.execute("SELECT count(npcgewonnen) FROM hs")
         myresult = mycursor.fetchall()
 
         for x in myresult:
