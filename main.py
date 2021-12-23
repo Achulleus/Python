06566c67a7c6501ce92c9489c099fe27ed56858d
Achilleus <56840977+Achulleus@users.noreply.github.com>
2021-12-23 12:14:55 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import random
 import mysql.connector
+import requests
+
+
+def sendRequest(username, voteScissors, voteRock, votePaper, voteSpock, voteLizard, apiIP="http://127.0.0.1:5000"):
+    reqUrl = apiIP + "/v1/updateRecord"
+    reqUrl += "?username=" + str(username) + "&voteScissors=" + str(voteScissors)
+    reqUrl += "&voteRock=" + str(voteRock) + "&votePaper=" + str(votePaper)
+    reqUrl += "&voteSpock=" + str(voteSpock) + "&voteLizard=" + str(voteLizard)
+    responseCode = 0
+    try:
+        response = requests.post(reqUrl, None)
+        responseCode = response.status_code
+    except:
+        return 0
+    return responseCode
+
+voteSchere = 0
+voteStein = 0
+votePapier = 0
+voteEchse = 0
+voteSpock = 0
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -73,71 +94,76 @@ while True:
     show = input("\nSoll die Statistik ausgegeben werden? (j/n): ")
     if show == "j" or show == "n":
         print("Der Spieler hat so oft Spock genommen:")
-        mycursor.execute('SELECT count(spieler) FROM sspes where spieler = "spock";')
+        voteSpock = mycursor.execute('SELECT count(spieler) FROM sspes where spieler = "spock";')
         myresult = mycursor.fetchall()
 
         for x in myresult:
             print(x)
 
         print("Der Spieler hat so oft Schere genommen:")
-        mycursor.execute('SELECT count(spieler) FROM sspes where spieler = "schere";')
+        voteSchere = mycursor.execute('SELECT count(spieler) FROM sspes where spieler = "schere";')
         myresult = mycursor.fetchall()
 
         for x in myresult:
             print(x)
 
         print("Der Spieler hat so oft Stein genommen:")
-        mycursor.execute('SELECT count(spieler) FROM sspes where spieler = "stein";')
+        voteStein = mycursor.execute('SELECT count(spieler) FROM sspes where spieler = "stein";')
         myresult = mycursor.fetchall()
 
         for x in myresult:
             print(x)
 
         print("Der Spieler hat so oft Papier genommen:")
-        mycursor.execute('SELECT count(spieler) FROM sspes where spieler = "papier";')
+        votePapier = mycursor.execute('SELECT count(spieler) FROM sspes where spieler = "papier";')
         myresult = mycursor.fetchall()
 
         for x in myresult:
             print(x)
 
         print("Der Spieler hat so oft Echse genommen:")
-        mycursor.execute('SELECT count(spieler) FROM sspes where spieler = "echse";')
+        voteEchse = mycursor.execute('SELECT count(spieler) FROM sspes where spieler = "echse";')
         myresult = mycursor.fetchall()
 
         for x in myresult:
             print(x)
 
         print("Der Computer hat so oft Spock genommen:")
-        mycursor.execute('SELECT count(npc) FROM sspes where npc = "spock";')
+        temp = mycursor.execute('SELECT count(npc) FROM sspes where npc = "spock";')
         myresult = mycursor.fetchall()
+        voteSpock = voteSpock + temp
 
         for x in myresult:
             print(x)
 
         print("Der Computer hat so oft Schere genommen:")
-        mycursor.execute('SELECT count(npc) FROM sspes where npc = "schere";')
+        temp = mycursor.execute('SELECT count(npc) FROM sspes where npc = "schere";')
         myresult = mycursor.fetchall()
+        voteSchere = voteSchere + temp
 
         for x in myresult:
             print(x)
 
         print("Der Cmputer hat so oft Stein genommen:")
-        mycursor.execute('SELECT count(npc) FROM sspes where npc = "stein";')
+        temp = mycursor.execute('SELECT count(npc) FROM sspes where npc = "stein";')
         myresult = mycursor.fetchall()
+        voteStein = voteStein + temp
 
         for x in myresult:
             print(x)
 
         print("Der Computer hat so oft Papier genommen:")
-        mycursor.execute('SELECT count(npc) FROM sspes where npc = "papier";')
+        temp = mycursor.execute('SELECT count(npc) FROM sspes where npc = "papier";')
         myresult = mycursor.fetchall()
+        votePapier = votePapier + temp
 
         for x in myresult:
             print(x)
 
         print("Der Computer hat so oft Echse genommen:")
-        mycursor.execute('SELECT count(npc) FROM sspes where npc = "echse";')
+        temp = mycursor.execute('SELECT count(npc) FROM sspes where npc = "echse";')
         myresult = mycursor.fetchall()
+        voteEchse = voteEchse + temp
 
         for x in myresult:
             print(x)
@@ -160,5 +186,7 @@ while True:
     if again == "J" or again == "j":
         continue
     else:
+        print(sendRequest("Achulleus", voteSchere, voteStein, votePapier, voteSpock, voteEchse))
         mydb.close()
         break
+
