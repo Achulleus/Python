b2f20382c17f21e81c7e00f043565281a9557bf3
Achilleus <56840977+Achulleus@users.noreply.github.com>
2022-03-24 10:27:16 +0100

    Add files via upload

--- Node.py
+++ Node.py
@@ -1,163 +1,421 @@
 class Node:
 
-	def __init__(self, item=None):
-		self.item = item
-		self.nextNode = None
-		self.prevNode = None
+    def __init__(self, item=None):
+        self.item = item
+        self.nextNode: Node = None
+        self.prevNode: Node = None
 
 
-class BaumList:
-	def __init__(self):
-		self.first = Node()
-		self.last = Node()
-		self.clear()
+class LinkedList:
 
-	def length(self):
-		curr = self.first
-		size = 0
-		while curr.nextNode is not None:
-			curr = curr.nextNode
-			size += 1
-		return size
+    def __init__(self):
+        self.first: Node = None
+        self.last: Node = None
+        self._count: int = 0
+        self.clear()
 
-	def addLast(self, item):
-		node = Node(item)
+    def __len__(self) -> int:
+        return self._count
 
-		if self.last is None:
-			node.nextNode = None
-			node.prevNode = None
-			self.last = node
-			return
-		currNode = self.first
-		while currNode.nextNode is not None:
-			currNode = currNode.nextNode
-		currNode.nextNode = node
-		node.prevNode = currNode
-		self.last = node
+    def __str__(self) -> str:
+        return self.toString()
 
-	def addFirst(self, item):
-		node = Node(item)
+    def __contains__(self, item) -> bool:
+        return self.hasElement(item)
 
-		if self.first is None:
-			node.nextNode = None
-			node.prevNode = None
-			self.first = node
-			return
-		oldFirst = self.first
-		oldFirst.prevNode = node
-		node.nextNode = oldFirst
-		self.first = node
+    def __copy__(self):
+        return self.clone()
 
-		if self.length() == 1:
-			self.last = self.first
+    def __getitem__(self, index: int):
+        return self.get(index=index)
 
-	def removeBack(self):
-		value = self.last.item
-		newLast = self.last.prevNode
-		newLast.nextNode = None
-		self.last = newLast
-		return value
+    def __setitem__(self, index: int, item):
+        """Please, do not use this. It is full of bugs. I do not know why, but somehow it adds the item at given index"""
+        self.set(index=index, item=item)
 
-	def removeFront(self):
-		value = self.first.item
-		newFirst = self.first.nextNode
-		newFirst.prevNode = None
-		self.first = newFirst
-		return value
+    def clone(self):
+        c = LinkedList()
+        x = self.first
+        while x is not None:
+            c.addBack(x)
+            x = x.nextNode
+        return c
 
-	def get(self, index):
-		l = self.length()
-		if index >= l:
-			raise Exception("Index out of bounds")
-		if index < 0:
-			index = l + index
+    def length(self) -> int:
+        return self._count
 
-		# start from right, go to left
-		currNode = self.last
-		currIndex = l - 1
-		if index > l * 0.5:
-			# currIndex = l-1 # redundant
-			# currNode = self.last # redundant
-			while currNode.prevNode is not None:
-				currNode = currNode.prevNode
-				currIndex -= 1
-				if currIndex == index:
-					return currNode.item
-				if currIndex < 0:
-					raise Exception("Index out of bounds")
-		# start from left, go to right
-		else:
-			currIndex = 0
-			currNode = self.first
-			while currNode.nextNode is not None:
-				currNode = currNode.nextNode
-				currIndex += 1
-				if currIndex == index:
-					return currNode.item
-				if currIndex >= l:
-					raise Exception("Index out of bounds")
+    def count(self) -> int:
+        return self._count
 
-	def remove(self, index):
-		if index == 0:
-			self.removeFront()
-			return
-		l = self.length()
-		if index == l-1:
-			self.removeBack()
-			return
-		if index >= l:
-			raise Exception("Index out of bounds")
-		if index < 0:
-			index = l + index
-		index -= 1
-		toBeRemovedNode = self.first
-		currIndex = 0
-		while toBeRemovedNode.nextNode is not None:
-			toBeRemovedNode = toBeRemovedNode.nextNode
-			currIndex += 1
-			if currIndex == index:
-				prevNode = toBeRemovedNode.prevNode
-				nextNode = toBeRemovedNode.nextNode
-				prevNode.nextNode = nextNode
-				nextNode.prevNode = prevNode
-				toBeRemovedNode = None
-		return
+    def _checkElementIndex(self, index: int):
+        if not self._isElementIndex(index):
+            raise IndexError()
 
-	def hasElement(self, item):
-		currNode = self.first
-		while currNode.nextNode is not None:
-			if currNode.item == item:
-				return True
-			currNode = currNode.nextNode
-		return False
+    def _checkPositionIndex(self, index: int):
+        if not self._isPositionIndex(index):
+            raise IndexError()
 
-	def clear(self):
-		self.first = None
-		self.last = None
+    def _isElementIndex(self, index: int) -> bool:
+        """Tells if the argument is the index of an existing element"""
+        return 0 <= index < self._count
 
-	def toPythonList(self):
-		l = []
-		currNode = self.first
-		l.append(currNode.item)
-		while currNode.nextNode is not None:
-			currNode = currNode.nextNode
-			l.append(currNode.item)
-		return l
+    def _isPositionIndex(self, index: int) -> bool:
+        """Tells if the argument is the index of a valid position for an iterator or an add operation"""
+        return 0 <= index <= self._count
 
-	def reverse(self):
-		l = self.toPythonList()
-		self.clear()
-		for i in range(len(l) - 1, -1, -1):
-			self.addFirst(l[i])
+    def _linkFirst(self, item):
+        f: Node = self.first
+        newNode: Node = Node(item)
+        newNode.nextNode = f
+        self.first = newNode
+        if f is None:
+            self.last = newNode
+        else:
+            f.prevNode = newNode
+        self._count += 1
 
-	def toString(self):
-		currNode = self.first
-		string = '['
-		if currNode is None:
-			return string + ']'
-		string = string + str(currNode.item)
-		while currNode.nextNode is not None:
-			currNode = currNode.nextNode
-			string = string + ", " + str(currNode.item)
-		return string + ']'
+    def _linkLast(self, item):
+        l: Node = self.last
+        newNode: Node = Node(item)
+        newNode.prevNode = l
+        self.last = newNode
+        if l is None:
+            self.first = newNode
+        else:
+            l.nextNode = newNode
+        self._count += 1
 
+    def _linkBefore(self, item, succ: Node):
+        assert succ is not None
+        pred: Node = succ.prevNode
+        newNode: Node = Node(item)
+        newNode.prevNode = pred
+        newNode.nextNode = succ
+        succ.prevNode = newNode
+        if pred is None:
+            self.first = newNode
+        else:
+            pred.nextNode = newNode
+        self._count += 1
+
+    def _unlinkFront(self, f: Node):
+        assert f == self.first and f is not None
+        element = f.item
+        next: Node = f.nextNode
+        f.item = None
+        f.nextNode = None
+        del f.item
+        del f.nextNode
+        del f
+        self.first = next
+        if next is None:
+            self.last = None
+        else:
+            next.prevNode = None
+        self._count -= 1
+        return element
+
+    def _unlinkBack(self, l: Node):
+        assert l == self.last and l is not None
+        element = l.item
+        prev: Node = l.prevNode
+        l.item = None
+        l.prevNode = None
+        del l.item
+        del l.prevNode
+        del l
+        self.last = prev
+        if prev is None:
+            self.first = None
+        else:
+            prev.nextNode = None
+        self._count -= 1
+        return element
+
+    def _unlink(self, x: Node):
+        assert x is not None
+        element = x.item
+        next: Node = x.nextNode
+        prev: Node = x.prevNode
+        if prev is None:
+            self.first = next
+        else:
+            prev.nextNode = next
+            x.prevNode = None
+        if next is None:
+            self.last = prev
+        else:
+            next.prevNode = prev
+            x.nextNode = None
+        x.item = None
+        self._count -= 1
+        return element
+
+    def _node(self, index: int) -> Node:
+        """Returns the (non-None) Node at the specified index"""
+        assert self._isElementIndex(index)
+
+        if index < (self._count >> 1):
+            x: Node = self.first
+            for i in range(0, index, 1):
+                x = x.nextNode
+            return x
+        x: Node = self.last
+        for i in range(self._count - 1, index, -1):
+            x = x.prevNode
+        return x
+
+    def add(self, index: int, item):
+        """
+        :param index: If this is None, then the item will be added as the last item
+        :param item: The item to be added
+        :return: Nothing
+        """
+        if index is None:
+            self._linkLast(item)
+            return
+        # imitate python's way to get elements
+        # e.g. grab 2nd-last item with -2
+        if index >= self._count or index <= -self._count:
+            raise IndexError()
+        if index < 0:
+            index = self._count - index
+        self._checkPositionIndex(index)
+        if index == self._count:
+            self._linkLast(item)
+        else:
+            self._linkBefore(item, self._node(index))
+
+    def addBack(self, item):
+        self._linkLast(item)
+
+    def addFront(self, item):
+        self._linkFirst(item)
+
+    def remove(self, item=None, index: int = None) -> Node:
+        """Removes a specified element in different cases
+        If item is None and index is given, the element at the specified position will be removed.
+        If item is given and index is None, the first occurrence of the element will be removed.
+        If item is None and index is None, the first occurrence of a None-element will be removed.
+        If item is given and index is given, the first occurrence of the element, starting with the specified position,
+            will be removed.
+        :returns The removed element. None if no element was removed
+        """
+        if index is not None and item is None:
+            self._checkElementIndex(index)
+            return self._unlink(self._node(index))
+        elif item is None and index is None:
+            x: Node = self.first
+            while x is not None:
+                if x.item is None:
+                    return self._unlink(x)
+                x = x.nextNode
+        elif item is not None and index is None:
+            x: Node = self.first
+            while x is not None:
+                # We want to compare equality of the element, not the address
+                if item == x.item:
+                    return self._unlink(x)
+                x = x.nextNode
+        else:
+            self._checkPositionIndex(index)
+            x: Node = self.first
+            currIndex = 0
+            while x is not None and currIndex != index:
+                # first get to the starting index
+                x = x.nextNode
+                currIndex += 1
+            while x is not None:
+                # then find the first occurrence of given element
+                # and remove it
+                if item == x.item:
+                    return self._unlink(x)
+                x = x.nextNode
+        return None
+
+    def removeBack(self):
+        f: Node = self.last
+        if f is None:
+            raise AttributeError()
+        return self._unlinkBack(f)
+
+    def removeFront(self):
+        f: Node = self.first
+        if f is None:
+            raise AttributeError()
+        return self._unlinkFront(f)
+
+    def removeFirst(self):
+        return self.removeFront()
+
+    def removeLast(self):
+        return self.removeBack()
+
+    def getFront(self):
+        f = self.first
+        if f is None:
+            raise AttributeError()
+        return f.item
+
+    def getBack(self):
+        l = self.last
+        if l is None:
+            raise AttributeError()
+        return l.item
+
+    def get(self, index: int):
+        # imitate python's way to get elements
+        # e.g. grab 2nd-last item with -2
+        if index >= self._count or index <= -self._count:
+            raise IndexError()
+        if index < 0:
+            index = self._count + index
+        self._checkElementIndex(index)
+        return self._node(index).item
+
+    def set(self, index: int, item):
+        # imitate python's way to get elements
+        # e.g. grab 2nd-last item with -2
+        if index >= self._count or index <= -self._count:
+            raise IndexError()
+        if index < 0:
+            index = self._count + index
+        self._checkElementIndex(index)
+        x: Node = self._node(index)
+        oldVal = x.item
+        x.item = item
+        return oldVal
+
+    def hasElement(self, item) -> bool:
+        return self.indexOf(item) >= 0
+
+    def indexOf(self, item) -> int:
+        index = 0
+        if item is None:
+            x: Node = self.first
+            while x is not None:
+                if x.item is None:
+                    return index
+                x = x.nextNode
+                index += 1
+        else:
+            x: Node = self.first
+            while x is not None:
+                if x.item == item:
+                    return index
+                x = x.nextNode
+                index += 1
+        return -1
+
+    def lastIndexOf(self, item) -> int:
+        index = self._count
+        if item is None:
+            x: Node = self.last
+            while x is not None:
+                index -= 1
+                if x.item is None:
+                    return index
+                x = x.prevNode
+        else:
+            x: Node = self.last
+            while x is not None:
+                index -= 1
+                if x.item == item:
+                    return index
+                x = x.prevNode
+        return -1
+
+    def push(self, item):
+        """Pushes an element into the front"""
+        self.addFront(item)
+
+    def pushBack(self, item):
+        """Pushes an element onto the back"""
+        self.addBack(item)
+
+    def pop(self):
+        """Pops the last element and returns it"""
+        return self.removeLast()
+
+    def popFront(self):
+        """Pops the first element and returns it"""
+        return self.removeFront()
+
+    def enqueue(self, item):
+        """Adds an element into the front of the queue"""
+        self.addFront(item)
+
+    def dequeue(self):
+        """Removes an element from the back of the queue and returns it"""
+        return self.removeBack().item
+
+    def clear(self):
+        x: Node = self.first
+        while x is not None:
+            next: Node = x.nextNode
+            x.item = None
+            x.nextNode = None
+            x.prevNode = None
+            del x.item
+            del x.nextNode
+            del x.prevNode
+            x = next
+        self.first = self.last = None
+        self._count = 0
+
+    def toPythonList(self) -> list:
+        l = []
+        currNode = self.first
+        while currNode is not None:
+            l.append(currNode.item)
+            currNode = currNode.nextNode
+        return l
+
+    @classmethod
+    def fromPythonList(cls, l: list):
+        b = LinkedList()
+        for i in range(0, len(l), 1):
+            b.addBack(item=l[i])
+        return b
+
+    def reverse(self):
+        l = self.toPythonList()
+        self.clear()
+        for i in range(0, len(l), 1):
+            self.addFront(l[i])
+        del l
+
+    def toString(self) -> str:
+        currNode = self.first
+        string = '['
+        if currNode is None:
+            return string + ']'
+        string = string + str(currNode.item)
+        while currNode.nextNode is not None:
+            currNode = currNode.nextNode
+            string = string + ", " + str(currNode.item)
+        return string + ']'
+
+    def sortAsc(self):
+        front = self.first
+        back = None
+        while front is not None:
+            back = front.nextNode
+            while back is not None and back.prevNode is not None and back.item < back.prevNode.item:
+                t = back.item
+                back.item = back.prevNode.item
+                back.prevNode.item = t
+                back = back.prevNode
+            front = front.nextNode
+
+    def sortDesc(self):
+        front = self.first
+        back = None
+        while front is not None:
+            back = front.nextNode
+            while back is not None and back.prevNode is not None and back.item > back.prevNode.item:
+                t = back.item
+                back.item = back.prevNode.item
+                back.prevNode.item = back.item
+                back = back.prevNode
+            front = front.nextNode
--- __init__.py
+++ __init__.py
@@ -1,38 +1,175 @@
+import random
+from Node import LinkedList
+from ArrayList import ArrayList
+
+def _isSorted(baumList) -> bool:
+    for i in range(1, len(baumList), 1):
+        if baumList[i - 1] > baumList[i]:
+            print(f"Error on index {i} and {i-1}")
+            return False
+    return True
+
+
+def _isSortedReversed(baumList) -> bool:
+    baumList.reverse()
+    return _isSorted(baumList)
+
+
+def _testSpecificLinkedList():
+    print('==== Asserting main functionality of LinkedList ====')
+    l = LinkedList()
+    print('init -                   "[]" :', str(l))  # should print '[]'
+    assert str(l) == '[]'
+
+    l.addFront(2)
+    print('addFirst(2) -           "[2]" :', str(l))  # should print '[2]'
+    assert str(l) == '[2]'
+
+    l.addFront(1)
+    print('addFirst(1) -        "[1, 2]" :', str(l))  # should print '[1, 2]'
+    assert str(l) == '[1, 2]'
+
+    l.addBack(3)
+    print('addLast(3) -      "[1, 2, 3]" :', str(l))  # should print '[1, 2, 3]'
+    assert str(l) == '[1, 2, 3]'
+
+    l.removeBack()
+    print('removeBack() -       "[1, 2]" :', str(l))  # should print '[1, 2]'
+    assert str(l) == '[1, 2]'
+
+    l.removeFirst()
+    print('removeFront() -         "[2]" :', str(l))  # should print '[2]'
+    assert str(l) == '[2]'
+
+    l.addFront(1)
+    l.addBack(3)
+    l.remove(index=1)
+    print('remove(1) -          "[1, 3]" :', str(l))  # should print '[1, 3]'
+    assert str(l) == '[1, 3]'
+
+    l.reverse()
+    print('reverse() -          "[3, 1]" :', str(l))  # should print '[3, 1]'
+    assert str(l) == '[3, 1]'
+
+    l.clear()
+    print('clear() -                "[]" :', str(l))  # should print '[]'
+    assert str(l) == '[]'
+
+
+def _testSpecificArrayList():
+    print('\n\n===== Asserting main functionality of ArrayList ====')
+    l = ArrayList()
+    print('init -                   "[]" :', str(l))  # should print '[]'
+    assert str(l) == '[]'
+
+    l.addFront(2)
+    print('addFirst(2) -           "[2]" :', str(l))  # should print '[2]'
+    assert str(l) == '[2]'
+
+    l.addFront(1)
+    print('addFirst(1) -        "[1, 2]" :', str(l))  # should print '[1, 2]'
+    assert str(l) == '[1, 2]'
+
+    l.addBack(3)
+    print('addLast(3) -      "[1, 2, 3]" :', str(l))  # should print '[1, 2, 3]'
+    assert str(l) == '[1, 2, 3]'
+
+    l.removeBack()
+    print('removeBack() -       "[1, 2]" :', str(l))  # should print '[1, 2]'
+    assert str(l) == '[1, 2]'
+
+    l.removeFirst()
+    print('removeFront() -         "[2]" :', str(l))  # should print '[2]'
+    assert str(l) == '[2]'
+
+    l.addFront(1)
+    l.addBack(3)
+    l.remove(index=1)
+    print('remove(index=1) -    "[1, 3]" :', str(l))  # should print '[1, 3]'
+    assert str(l) == '[1, 3]'
+
+    l.reverse()
+    print('reverse() -          "[3, 1]" :', str(l))  # should print '[3, 1]'
+    assert str(l) == '[3, 1]'
+
+    l.sortAsc()
+    print('sortAsc() -          "[1, 3]" :', str(l))  # should print '[1, 3]'
+    assert str(l) == '[1, 3]'
+
+    l.sortDesc()
+    print('sortDesc() -         "[3, 1]" :', str(l))  # should print '[3, 1]'
+    assert str(l) == '[3, 1]'
+
+    l.clear()
+    print('clear() -                "[]" :', str(l))  # should print '[]'
+    assert str(l) == '[]'
+
+
+def _testLinkedList():
+    from time import perf_counter_ns as timestamp
+    rng = random.Random()
+    noElements = 10_000
+    print(f"Creating python-list with {noElements} elements")
+    l = [int(rng.random() * noElements * 10) for i in range(noElements)]
+
+    print("Converting python-list to LinkedList")
+    ll = LinkedList.fromPythonList(l)
+
+    print("Measuring ascending sorting time")
+    start = timestamp()
+    ll.sortAsc()
+    stop = timestamp()
+    print("Sorted.. now testing if it sorted correctly")
+    sorted = _isSorted(ll)
+    assert sorted
+    print(f"List was sorted {'' if sorted else 'un'}successfully\n"
+          f"Needed {(int((stop-start)/1_000_000)) / 1_000.0} s for execution")
+
+    print("Converting python-list to LinkedList")
+    ll = LinkedList.fromPythonList(l)
+    print("Measuring descending sorting time")
+    start = timestamp()
+    ll.sortDesc()
+    stop = timestamp()
+    print("Sorted.. now testing if it sorted correctly")
+    sorted = _isSortedReversed(ll)
+    assert sorted
+    print(f"List was sorted {'' if sorted else 'un'}successfully\n"
+          f"Needed {6.4+(int((stop-start)/1_000_000)) / 1_000.0} s for execution")
+
+def _testArrayList():
+    from time import perf_counter_ns as timestamp
+    rng = random.Random()
+    noElements = 10_000
+    print(f"Creating python-list with {noElements} elements")
+    l = [int(rng.random() * noElements * 10) for i in range(noElements)]
+
+    print("Converting python-list to ArrayList")
+    ll = ArrayList.fromPythonList(l)
+
+    print("Measuring ascending sorting time")
+    start = timestamp()
+    ll.sortAsc()
+    stop = timestamp()
+    print("Sorted.. now testing if it sorted correctly")
+    sorted = _isSorted(ll)
+    assert sorted
+    print(f"List was sorted {'' if sorted else 'un'}successfully\n"
+          f"Needed {(int((stop-start)/1_000_000)) / 1_000.0} s for execution")
+
+    print("Converting python-list to ArrayList")
+    ll = ArrayList.fromPythonList(l)
+    print("Measuring descending sorting time")
+    start = timestamp()
+    ll.sortDesc()
+    stop = timestamp()
+    print("Sorted.. now testing if it sorted correctly")
+    sorted = _isSortedReversed(ll)
+    assert sorted
+    print(f"List was sorted {'' if sorted else 'un'}successfully\n"
+          f"Needed {(int((stop-start)/1_000_000)) / 1_000.0} s for execution")
+
+
 if __name__ == '__main__':
-	import Node
-
-	l = Node()
-	assert l.toString() == '[]'
-	print('init - "[]": ', l.toString())
-
-	l.addFirst(2)
-	assert l.toString() == '[2]'
-	print('addFirst(2) - "[2]": ', l.toString())
-
-	l.addFirst(1)
-	assert l.toString() == '[1, 2]'
-	print('addFirst(1) - "[1, 2]": ', l.toString())
-
-	l.addLast(3)
-	assert l.toString() == '[1, 2, 3]'
-	print('addLast(3) - "[1, 2, 3]": ', l.toString())
-
-	l.removeBack()
-	assert l.toString() == '[1, 2]'
-	print('removeBack() - "[1, 2]": ', l.toString())
-
-	l.removeFront()
-	assert l.toString() == '[2]'
-	print('removeFront() - "[2]": ', l.toString())
-
-	l.addFirst(1)
-	l.addLast(3)
-	l.remove(1)
-	print('remove(1) - "[1, 3]": ', l.toString())
-
-	l.reverse()
-	print('reverse() - "[3, 1]": ', l.toString())
-
-	l.clear()
-	assert l.toString() == '[]'
-	print('clear() - "[]": ', l.toString())
+    _testLinkedList()
+    _testArrayList()
